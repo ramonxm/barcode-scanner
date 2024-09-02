8b8cfeaa2f6d5f65b97380be1cb08fd3768abb61
Ramon Xavier <ramon1234rxm@gmail.com>
2024-09-02 19:15:33 -0300

    initial commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Ramon Xavier on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,33 @@
+//
+//  BarcodeScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Ramon Xavier on 01/09/24.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+               Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Label("Barcode", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .padding()
+                    .foregroundStyle(.green)
+
+            }
+        }.navigationTitle("Barcode Scanner")
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
