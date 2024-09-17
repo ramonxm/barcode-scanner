aae36cfc20b27a72190cc241380f38960f8edb57
Ramon Xavier <ramon1234rxm@gmail.com>
2024-09-16 21:39:48 -0300

    feat: create scanner view

---
+++ BarcodeScanner/ScannerView.swift
@@ -0,0 +1,49 @@
+//
+//  ScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Ramon Xavier on 16/09/24.
+//
+
+import SwiftUI
+
+struct ScannerView: UIViewControllerRepresentable {
+    
+    @Binding var scannedCode: String
+
+    func makeUIViewController(context: Context) -> ScannerVC {
+        ScannerVC(scannerDelegate: context.coordinator)
+    }
+    
+    func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(scannerView: self)
+    }
+    
+    final class Coordinator: NSObject, ScannerVCDelegate {
+        
+        private let scannerView: ScannerView
+        
+        init(scannerView: ScannerView) {
+            self.scannerView = scannerView
+        }
+        
+        func didFind(barcode: String) {
+            scannerView.scannedCode = barcode
+        }
+        
+        func didSurface(error: CameraError) {
+            print(error.rawValue)
+        }
+        
+        
+    }
+    
+    
+}
+
+#Preview {
+    ScannerView(scannedCode: .constant("1234567"))
+}
